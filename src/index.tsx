271eae0d33f6a6ed51fe2e7687fc0dfbee8ea602
sunset0917 <159934376+sunset0917@users.noreply.github.com>
2026-09-12 11:48:27 -0500

    Change root element ID from 'app' to 'root'

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import {createRoot} from "react-dom/client";
 import {Home} from "./screens/Home/Home"
 
-createRoot(document.getElementById("app") as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
     <StrictMode>
         <Home />
     </StrictMode>,
